dfa5e1bfc302c8ac0d8967b819825b36d74c5ad8
Adrien LAUER <adrien.lauer@gmail.com>
2017-07-19 22:02:20 +0200

    Copied from business framework

---
+++ README.md
@@ -0,0 +1,10 @@
+# SeedStack ModelMapper add-on
+
+[![Build status](https://travis-ci.org/seedstack/modelmapper-addon.svg?branch=master)](https://travis-ci.org/seedstack/modelmapper-addon) [![Coverage Status](https://coveralls.io/repos/seedstack/modelmapper-addon/badge.svg?branch=master)](https://coveralls.io/r/seedstack/modelmapper-addon?branch=master) [![Maven Central](https://maven-badges.herokuapp.com/maven-central/org.seedstack.addons.modelmapper/modelmapper/badge.svg?style=flat)](https://maven-badges.herokuapp.com/maven-central/org.seedstack.addons.modelmapper/modelmapper)
+
+Provides business framework automatic assemblers based on the open-source library ModelMapper.
+
+# Copyright and license
+
+This source code is copyrighted by [The SeedStack Authors](https://github.com/seedstack/seedstack/blob/master/AUTHORS) and
+released under the terms of the [Mozilla Public License 2.0](https://www.mozilla.org/MPL/2.0/). 
---
+++ src/main/java/org/seedstack/business/modelmapper/ModelMapperAssembler.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2013-2016, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.seedstack.business.modelmapper;
+
+import org.modelmapper.ModelMapper;
+import org.seedstack.business.assembler.BaseAssembler;
+import org.seedstack.business.domain.AggregateRoot;
+import org.seedstack.seed.Application;
+import org.seedstack.seed.ClassConfiguration;
+
+import javax.inject.Inject;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * This assembler automatically assembles aggregates in DTO and vice versa.
+ *
+ * @param <A> the aggregate root
+ * @param <D> the dto
+ */
+public abstract class ModelMapperAssembler<A extends AggregateRoot<?>, D> extends BaseAssembler<A, D> {
+    private final AtomicBoolean assembleConfigured = new AtomicBoolean(false);
+    private final AtomicBoolean mergeConfigured = new AtomicBoolean(false);
+    private final ModelMapper assembleModelMapper = new ModelMapper();
+    private final ModelMapper mergeModelMapper = new ModelMapper();
+    @Inject
+    private Application application;
+
+    public ModelMapperAssembler() {
+        super();
+    }
+
+    protected ModelMapperAssembler(Class<D> dtoClass) {
+        super(dtoClass);
+    }
+
+    @Override
+    public D createDtoFromAggregate(A sourceAggregate) {
+        if (!assembleConfigured.getAndSet(true)) {
+            configureModelMapper(assembleModelMapper);
+            configureAssembly(assembleModelMapper);
+        }
+        return assembleModelMapper.map(sourceAggregate, getDtoClass());
+    }
+
+    @Override
+    public void mergeAggregateIntoDto(A sourceAggregate, D targetDto) {
+        if (!assembleConfigured.getAndSet(true)) {
+            configureModelMapper(assembleModelMapper);
+            configureAssembly(assembleModelMapper);
+        }
+        assembleModelMapper.map(sourceAggregate, targetDto);
+    }
+
+    @Override
+    public void mergeDtoIntoAggregate(D sourceDto, A targetAggregate) {
+        if (!mergeConfigured.getAndSet(true)) {
+            configureModelMapper(mergeModelMapper);
+            configureMerge(mergeModelMapper);
+        }
+        mergeModelMapper.map(sourceDto, targetAggregate);
+    }
+
+    private void configureModelMapper(ModelMapper assembleModelMapper) {
+        //ClassConfiguration<D> dtoConfiguration = application.getConfiguration(getDtoClass());
+    }
+
+    protected abstract void configureAssembly(ModelMapper modelMapper);
+
+    protected abstract void configureMerge(ModelMapper modelMapper);
+}
